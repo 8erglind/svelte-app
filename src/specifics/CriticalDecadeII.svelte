d1a2ecadba1ce6af2e6f59a1cceb4c4053266e49
berglindbra <41021844+8erglind@users.noreply.github.com>
2020-06-10 23:28:16 +0200

    Update CriticalDecadeII.svelte

--- src/specifics/CriticalDecadeII.svelte
+++ src/specifics/CriticalDecadeII.svelte
@@ -1,34 +1,280 @@
 <script>
 	import TimelinePast from './TimelinePast.svelte';
+	import TimelineFuture from './TimelineFuture.svelte';
+	export let pagetitleText;
+	export let rotate;
+
 
 	let distanceBLines = 'calc((95vh - 1px) / 9 * 1)';
 	let marginSides = 'calc(100vw / 16)';
-	
-	export let pagetitleText;
-	export let rotate;
+
+	let tempWidthA = 'calc((100vw - (100vw / 8)) / 15 * 12)';
+	let tempWidthB = 'calc(((100vw - (100vw / 8)) / 15 * 10) / 3 * 2)';
+
+
+	let firstText = true;
+	let secondText = false;
+	let thirdText = false;
+	let fourthText = false;
+	let fifthText = false;
+
+	let emissionGraph = false;
+	let fullMeter = false;
+	let halfMeter = false;
+	let year20 = false;
+	let line50 = false;
+	let line30 = false;
+	let sixthText = false;
+
+
+	const togglefirstSetup = () => {
+		firstText = true;
+		secondText = false;
+		thirdText = false;
+		fourthText = false;
+		fifthText = false;
+
+		emissionGraph = false;
+		fullMeter = false;
+		halfMeter = false;
+		line50 = false;
+		line30 = false;
+		year20 = false;
+		sixthText = false;
+	}
+
+	const togglesecondSetup = () => {
+		firstText = false;
+		secondText = true;
+		thirdText = false;
+		fourthText = false;
+		fifthText = false;
+
+		emissionGraph = true;
+		fullMeter = true;
+		halfMeter = false;
+		line50 = false;
+		line30 = false;
+		year20 = false;
+		sixthText = false;
+	}
+
+	const togglethirdSetup = () => {
+		firstText = false;
+		secondText = false;
+		thirdText = true;
+		fourthText = false;
+		fifthText = false;
+
+		emissionGraph = true;
+		fullMeter = true;
+		halfMeter = false;
+		line50 = false;
+		line30 = false;
+		year20 = true;
+		sixthText = false;
+	}
+
+	const togglefourthSetup = () => {
+		firstText = false;
+		secondText = false;
+		thirdText = false;
+		fourthText = true;
+		fifthText = false;
+
+		emissionGraph = true;
+		fullMeter = false;
+		halfMeter = false;
+		line50 = true;
+		line30 = false;
+		year20 = true;
+		sixthText = false;
+	}
+
+	const togglefifthSetup = () => {
+		firstText = false;
+		secondText = false;
+		thirdText = false;
+		fourthText = false;
+		fifthText = true;
+
+		emissionGraph = true;
+		fullMeter = false;
+		halfMeter = true;
+		line50 = true;
+		line30 = true;
+		year20 = true;
+		sixthText = false;
+	}
+
+	const togglesixthSetup = () => {
+		firstText = false;
+		secondText = false;
+		thirdText = false;
+		fourthText = false;
+		fifthText = true;
+
+		emissionGraph = true;
+		fullMeter = false;
+		halfMeter = true;
+		line50 = false;
+		line30 = true;
+		year20 = true;
+		sixthText = true;
+	}
+
 </script>
 
 
-<TimelinePast></TimelinePast>
+
+<!--    BUTTONS    -->
+
+
+{#if firstText}
+	<div class="buttonNext" on:click={togglesecondSetup}></div>
+	<a class="buttonPrev" href="#page4"></a>
+{/if}
+{#if secondText}
+	<div class="buttonNext" on:click={togglethirdSetup}></div>
+	<div class="buttonPrev" on:click={togglefirstSetup}></div>
+{/if}
+{#if thirdText}
+	<div class="buttonNext" on:click={togglefourthSetup}></div>
+	<div class="buttonPrev" on:click={togglesecondSetup}></div>
+{/if}
+{#if fourthText}
+	<div class="buttonNext" on:click={togglefifthSetup}></div>
+	<div class="buttonPrev" on:click={togglethirdSetup}></div>
+{/if}
+{#if fifthText}
+	<div class="buttonNext" on:click={togglesixthSetup}></div>
+	<div class="buttonPrev" on:click={togglefourthSetup}></div>
+{/if}
+{#if sixthText}
+	<a class="buttonNext" href="#page6"></a>
+	<div class="buttonPrev" on:click={togglefifthSetup}></div>
+{/if}
+
+
+
+
+
+
+
+
+
+<!--    TEXT    -->
+
 
 
 <div class="pagetitle" style="transform: rotate({rotate});">
  	{pagetitleText}
- 	<div class="text"></div>
+ 	{#if firstText}
+ 		<div class="text">Emissions have risen steadily since the industrial revolution. <span class="transp">With our current level of emissions we have reached the limit. From now on we must reduce.</span></div>
+ 	{/if}
+ 	{#if secondText}
+ 		<div class="text">Emissions have risen steadily since the industrial revolution. With our current level of emissions we have reached the limit. <span class="transp">From now on we must reduce.</span></div>
+ 	{/if}
+ 	{#if thirdText}
+ 		<div class="text">Emissions have risen steadily since the industrial revolution. With our current level of emissions we have reached the limit. From now on we must reduce.</div>
+ 	{/if}
+ 	{#if fourthText}
+ 		<div class="text">To limit warming to 1,5°C, global CO2 emissions must have reached net-zero by&nbsp;2050.</div>
+ 	{/if}
+ 	{#if fifthText}
+ 		<div class="text">To reach net-zero by 2050, emissions must be halved by 2030.</div>
+ 	{/if}
 </div>
 
 
 
-<!--<div class="tempMeter">
-	<div class="temperature">
-		<span class="tempnumber text">1,2°C</span>
+
+<!--    CONTENT    -->
+
+
+<div class="backgroundBox">
+	{#if emissionGraph}
+		<svg class="graph" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 365 748.07" preserveAspectRatio="none">
+			<polyline class="cls-1" points="365 748.07 0 748.07 0 0 68.47 0 78.61 94.09 111.59 187 148.35 280 194.96 374.03 224.21 467.1 252.83 561.07 331.85 654.03"/>
+		</svg>
+	{/if}
+	{#if fourthText}
+		<svg class="graph" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 365 748.07" preserveAspectRatio="none">
+			<polygon class="cls-2" points="365 748.07 0 748.07 0 467.1 365 748.07"/>
+		</svg>
+	{/if}
+	{#if fifthText}
+		<svg class="graph" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 365 748.07" preserveAspectRatio="none">
+			<polygon class="cls-2" points="365 748.07 0 748.07 0 467.1 182.5 654.03 365 748.07"/>
+		</svg>
+	{/if}
+</div>
+
+
+
+
+
+<div class="tempMeter">
+	{#if fullMeter}
+		<div class="temperature fullMeter"></div>
+	{/if}
+	{#if halfMeter}
+		<div class="temperature halfMeter"></div>
+	{/if}
+	<div class="temperature infotext">
+		<span class="tempnumber text">current level emissions</span>
+		<span class="tempnumber left text">net-zero</span>
 	</div>
 </div>
 
-<div class="verticalLine1"></div>
 
-<div class="arrow text">&uarr;</div>
--->
+{#if secondText}
+	<TimelinePast></TimelinePast>
+	<div class="text years left line0">2020</div>
+	<div class="arrow text" style="width: {marginSides};">&uarr;</div>
+{/if}
+
+{#if thirdText}
+	<TimelineFuture></TimelineFuture>
+{/if}
+
+{#if year20}
+	<div class="text years right line0">2020</div>
+{/if}
+
+
+{#if fourthText}
+	<div class="horizontalLine left" style="width: 100%; top: calc(({distanceBLines} * 6) - 1px);"></div>
+	<div class="text years right line30">2050</div>
+{/if}
+
+{#if line50}
+	<div class="line right line30"></div>
+{/if}
+{#if line30}
+	<div class="text years right line10">2030</div>
+	<div class="line right line10"></div>
+{/if}
+
+
+
+
+{#if fifthText}
+	<div class="horizontalLine left" style="width: 100%; top: calc(({distanceBLines} * 8) - 1px);"></div>
+{/if}
+{#if sixthText}
+	<div class="horizontalLine left" style="width: 100%; top: calc(({distanceBLines} * 8) - 1px); border-top: 1px solid blue;"></div>
+	<div class="text criticalText" style="left: {marginSides}; right: {marginSides}; top: calc(({distanceBLines} * 8);">This is a Critical Decade.</div>
+{/if}
+
+
+
+
+
+
+
+<!--    DOTS    -->
+
 
 <div class="activedot activedot5"></div>
 <div class="activedotnew activedotFan">
@@ -38,36 +284,57 @@
 
 
 
+
+
 <style>
 
-	.temperature {
-		position: absolute;
-		background-color: rgb(70,70,70);
-		background-color: lightgrey;
-		height: 100%;
-		top: 0%;
-		left: 0%;
-		width: calc((100% / 15) * 12);
-	}
-	.tempnumber {
-		position: absolute;
-		right: 2.5px;
-		top: 2.5px;
-		/*color: white !important;*/
+	.text {color: blue;}
+	.arrow {text-align: center;}
+	.line {background-color: blue;}
+	.backgroundBox {
+		background-color: rgb(245,245,245);
+		overflow: hidden; 
+		border-bottom-right-radius: 0px; 
+		border-left: 1px solid blue; 
+		border-right: 1px solid blue;
 	}
 
-	.verticalLine1 {
-		position: absolute;
-		top: 0vh;
-		height: 95vh;
-		width: 1px;
-		border-right: 1px dotted blue;
-		left: calc((100vw / 16) + (((100vw - ((100vw / 16) * 2)) / 15) * 12) - 1px - 0.5px);
+	.tempMeter {
+		border-left: 1px solid blue; 
+		border-right: 1px solid blue;
 	}
-	.text {color: blue;}
-	.arrow {
-		width: calc(100vw / 16);
+	.temperature {border: 1px dotted blue;}
+	.infotext {
+		width: 100%; 
+		background-color: rgba(0,0,0,0) !important; 
+		border: none;
+	}
+	.fullMeter {
+		width: 100%; 
+		background-color: rgba(230,230,230,1) !important; 
+		border: none; 
+		border-radius: 20px;
+	}
+	.halfMeter {
+		width: 50%; 
+		background-color: beige !important; 
+		border: none; 
+		border-bottom-left-radius: 20px; 
+		border-top-left-radius: 20px;
+	}
+	.verticalLine {border-right: 1px dotted blue;}
+	.horizontalLine {border-top: 1px dotted blue;}
+
+	.criticalText {
+		position: absolute; 
+		bottom: 0%;  
 		text-align: center;
+		/*background-color: rgba(0,0,255,.5);*/
+		color: blue;
+		font-size: 5.5vw;
+		padding-top: 10px;
+		text-transform: uppercase;
+		font: 5.5vw arial;
 	}
 
 
