72f388ec0074b2842e19ffe7d7dbdb515b291ff0
berglindbra <41021844+8erglind@users.noreply.github.com>
2020-06-11 10:33:52 +0200

    Update footer.svelte

--- src/footer.svelte
+++ src/footer.svelte
@@ -14,10 +14,10 @@
 		border-top: 1px solid rgb(70,70,70);
 		border-bottom: 1px solid rgb(70,70,70);
 		border-right: 1px solid rgb(70,70,70);
-		height: calc(5vh - 2px);
+		height: calc(5% - 2px);
 		width: calc(100vw - 1px);
 		left: 0vw;
-		bottom: 0vh;
+		bottom: 0%;
 		pointer-events: none;
 		z-index: -1;
 	}
